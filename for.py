30261e95008de477c26ca5aaa58346a853b387fb
Nikhil <Niikhiilmr@gmail.com>
2022-12-27 09:08:43 +0530

    first commit

--- for.py
+++ for.py
@@ -1,8 +1,31 @@
-files = ["thor.mp4", "iron man.mp4", "batman.mp4"]
-for x in files:
-    print(x[0:-4])
+import customtkinter
 
-print(files)
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("blue")
 
-for x in range(17, 100, 5):
-    print(x)
+root = customtkinter.CTk()
+root.geometry("500x350")
+
+
+def login():
+    print("Test")
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+label = customtkinter.CTkLabel(master=frame, text="Login System",font=("Lato", 24))
+label.pack(pady=12, padx=10)
+
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="username")
+entry1.pack(pady=12, padx=10) 
+
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
+entry2.pack(pady=12, padx=10)
+
+button = customtkinter.CTkButton(master=frame, text="Login", command=login)
+button.pack(pady=12, padx=10)
+
+checkbox = customtkinter.CTkCheckBox(master=frame, text="remember me")
+checkbox.pack(pady=12, padx=10)
+
+root.mainloop()
--- while_guessinggame.py
+++ while_guessinggame.py
@@ -5,7 +5,7 @@ guess_limit = 3
 out_of_guesses = False
 while guess != secret_word and not out_of_guesses:
     if guess_count < guess_limit:
-        guess = input("enter guess:  ")
+        guess = input(" guess the animal:  ")
         guess_count = guess_count + 1
     else:
         out_of_guesses = True
